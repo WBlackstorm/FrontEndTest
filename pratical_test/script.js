88bcbe4933d409d9d43e7350ea303a5ce6a65b8e
Weverton Peron <wevertonperon@MacBook-Pro-de-Weverton.local>
2023-02-28 21:02:06 -0300

    Implementing layout

--- pratical_test/script.js
+++ pratical_test/script.js
@@ -3,6 +3,7 @@ const dropArea = document.querySelector(".drag-area"),
     dragText = dropArea.querySelector("header"),
     button = dropArea.querySelector("button"),
     input = dropArea.querySelector("input");
+
 let file; //this is a global variable and we'll use it inside multiple functions
 dropArea.onclick = () => {
     input.click(); //if user click on the button then the input also clicked
